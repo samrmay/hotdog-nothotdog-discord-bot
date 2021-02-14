b42800bdc1137bea5d7eb41d273519c31141f688
Sam May <samrmay@sas.upenn.edu>
2021-02-14 14:44:44 -0500

    add predict image command to bot

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from discord.ext import commands
 import asyncio
 from dotenv import load_dotenv
 from PIL import Image
+import requests
 from hot_nothotdog_pred import predict_image
 
 load_dotenv()
 # Permissions num: 75776
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+MODEL_PATH = os.getenv('MODEL_PATH')
 bot = commands.Bot(command_prefix='!')
 
 
@@ -31,5 +33,32 @@ async def on_ready():
     # Print members of guild that bot has jsut connected to
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
+
+
+@bot.command(name="hotdog")
+async def hotdog_cmd(ctx):
+    async for message in ctx.channel.history(limit=200):
+        if (message.attachments != None and message.attachments != []):
+            src = message.attachments[0].url
+            break
+        elif message.content.startswith('https://cdn.discordapp.com/attachments/'):
+            src = message.content
+            break
+    print(src)
+    r = requests.get(src, stream=True, timeout=10)
+    with open(f"temp/temp_pic.png", 'wb') as output:
+        output.write(r.content)
+    img = Image.open('temp/temp_pic.png')
+    print(img)
+    prob = predict_image(img, MODEL_PATH)
+    try:
+        os.remove('temp/temp_pic.png')
+    except:
+        print("Error, temporary picture was already removed or never existed")
+    print(prob)
+    if prob == 1:
+        await ctx.send('\U0001F32D\U0000200D\U0000274C')
+    else:
+        await ctx.send('\U0001F32D')
 
 bot.run(TOKEN)
