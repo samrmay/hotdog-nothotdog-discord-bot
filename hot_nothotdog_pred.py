a30208755589216b86184dfdb0ab879a7df061bc
Sam May <samrmay@sas.upenn.edu>
2021-02-15 10:57:44 -0500

    Remove redundant data batching function

--- hot_nothotdog_pred.py
+++ hot_nothotdog_pred.py
@@ -33,23 +33,6 @@ def to_device(data, device):
     return data.to(device, non_blocking=True)
 
 
-class DeviceDataLoader():
-    """Wrap a dataloader to move data to a device"""
-
-    def __init__(self, dl, device):
-        self.dl = dl
-        self.device = device
-
-    def __iter__(self):
-        """Yield a batch of data after moving it to device"""
-        for b in self.dl:
-            yield to_device(b, self.device)
-
-    def __len__(self):
-        """Number of batches"""
-        return len(self.dl)
-
-
 device = get_default_device()
 
 
